a1445a2708ae8c9217417d56a796fe6aefba8afb
Nelson Da Silva <ndasilva115@gmail.com>
2023-04-20 15:23:55 +0100

    Adding the yaml file needed to train on all variations of FishDrone as well as a script that downloads FishDrone and merges the necessary data all together to form the combined dataset

---
+++ scripts/format_fishdrone.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# To be run from the yolov7 directory e.g.
+#   chmod /scripts/format_fishdrone.sh
+#   ./scripts/format_fishdrone.sh
+
+# Run the python script that downloads the Fishdrone dataset
+# python get_fishdrone.py
+
+dir="FishDrone"
+
+# Merge the oversized training images together into one folder
+# mv $dir/train-600-images-2/* $dir/train-600/images/
+# mv $dir/train-830-images-2/* $dir/train-830/images/
+
+#   Create a folder containing all images of the varying focal lengths together
+mkdir $dir/all/train/images
+mkdir $dir/all/train/labels
+
+mkdir $dir/all/test/images
+mkdir $dir/all/test/labels
+
+mkdir $dir/all/val/images
+mkdir $dir/all/val/labels
+
+# Rename while copying to the unified directory
+for g in $dir/train-* # Iterate through train folders
+do
+    fl="$(cut -d'-' -f2 <<<$g)" # Focal length
+    for f in $g/*/* # Iterate through both folders
+    do
+        type=$(basename $(dirname $f)) # image or label
+        name=$(basename "$f") # Get name of file to be copied over
+        cp -- "$f" "$dir/all/train/$type/f"$fl"_"$name # copy over with prefix
+    done
+done
+
+for g in $dir/test-* # Iterate through test folders
+do
+    fl="$(cut -d'-' -f2 <<<$g)" # Focal length
+    for f in $g/*/* # Iterate through both folders
+    do
+        type=$(basename $(dirname $f)) # image or label
+        name=$(basename "$f") # Get name of file to be copied over
+        cp -- "$f" "$dir/all/test/$type/f"$fl"_"$name # copy over with prefix
+    done
+done
+
+for g in $dir/val-* # Iterate through val folders
+do
+    fl="$(cut -d'-' -f2 <<<$g)" # Focal length
+    for f in $g/*/* # Iterate through both folders
+    do
+        type=$(basename $(dirname $f)) # image or label
+        name=$(basename "$f") # Get name of file to be copied over
+        cp -- "$f" "$dir/all/val/$type/f"$fl"_"$name # copy over with prefix
+    done
+done
+
+# Needed three separate for loops rather than having them all in one to avoid
+#   detecting the /all directory containing all labels combined
